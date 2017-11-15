172e4174ff08024cac858e6eb336e25d557425ce
danslavov <danslavov@gmail.com>
2017-11-15 15:42:51 +0200

    Problem 1

---
+++ src/_02_InterfacesAndAbstraction/Lab/L01_ShapesDrawing/Drawable.java
@@ -0,0 +1,5 @@
+package _02_InterfacesAndAbstraction.Lab.L01_ShapesDrawing;
+
+public interface Drawable {
+    void draw();
+}
---
+++ src/_02_InterfacesAndAbstraction/Lab/L01_ShapesDrawing/Rectangle.java
@@ -0,0 +1,30 @@
+package _02_InterfacesAndAbstraction.Lab.L01_ShapesDrawing;
+
+public class Rectangle implements Drawable {
+
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+        for (int i = 0; i < this.height; i++) {
+            System.out.print("*");
+            for (int j = 1; j < this.width - 1; j++) {
+                System.out.print(" ");
+                if (i == 0 || i == (this.height - 1)) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.print(" ");
+            System.out.print("*");
+            System.out.println();
+        }
+    }
+}
